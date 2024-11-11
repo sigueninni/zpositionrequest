2ef15a829497ae87e3fb6fbf91f7a06cee71af1e
Kywan Labs <67273768+sigueninni@users.noreply.github.com>
2024-11-11 12:11:59 +0100

    Init Detail View

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -48,6 +48,55 @@ sap.ui.define([], function () {
             } else {
                 return "None";
             }
+        },
+        flowText: function (iFlow) {
+            if (iFlow) {
+                switch (iFlow) {
+                    case 'c':
+                        return "Create";
+                    case 'u':
+                        return "Update";
+                    default:
+                        return "None";
+                }
+            } else {
+                return "None";
+            }
+
+        },
+
+        typeText: function (iType) {
+            if (iType) {
+                switch (iType) {
+                    case '01':
+                        return "position creation";
+                    case '02':
+                        return "position creation(temporary leave)";
+                    case '21':
+                        return "trigger recruitment";
+                    case '22':
+                        return "position extension";
+                    case '23':
+                        return "working time change";
+                    case '24':
+                        return "type of contract change";
+                    case '25':
+                        return "job/level change";
+                    case '26':
+                        return "range change";
+                    case '27':
+                        return "assignment change";
+                    case '28':
+                        return "on hold";
+                    case '29':
+                        return "otp change";
+                    default:
+                        return "None";
+                }
+            } else {
+                return "None";
+            }
+
         }
 
 
